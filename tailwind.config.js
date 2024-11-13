773afa246014b164178a01043469981a9763139b
Prince Ahmed <cwp@Princes-MBP.station>
2024-11-13 23:58:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./sections/**/*.liquid", "./snippets/**/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
